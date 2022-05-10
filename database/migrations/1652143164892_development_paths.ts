05b83f012e0168d172289ab5ac9d7d26d6869042
devalexandre <alexandre@indev.net.br>
2022-05-09 22:29:09 -0300

    feat: add migrations

---
+++ database/migrations/1652143164892_development_paths.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class DevelopmentPaths extends BaseSchema {
+  protected tableName = 'development_paths'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title', 100).notNullable()
+      table.text('description')
+      table.string('thumbnail')
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at',{ useTz: true })
+      table.timestamp('updated_at',{ useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
